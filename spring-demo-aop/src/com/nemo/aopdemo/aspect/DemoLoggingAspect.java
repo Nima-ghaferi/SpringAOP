aecd39875701993e12634803bfe3e12259e6d86d
nima ghaferi <nima.ghaferi@gmail.com>
2022-11-11 17:44:39 +0330

    Spring AOP Before point cut samples

--- spring-demo-aop/src/com/nemo/aopdemo/aspect/DemoLoggingAspect.java
+++ spring-demo-aop/src/com/nemo/aopdemo/aspect/DemoLoggingAspect.java
@@ -8,6 +8,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class DemoLoggingAspect {
     @Before("execution(boolean add*())")
+    //@Before("execution(boolean add*(java.lang.String))")----match with specific arg type
+    //@Before("execution(boolean add*())")-----match with no arg method
+    //@Before("execution(boolean add*(*))")-----match with one arg with any type
+    //@Before("execution(boolean add*(..))")-----match with any arg number with any types
+    //@Before("execution(* com.nemo.aopdemo.dao.*.* (..))")//-----match with any method in a specific class
+
+
     public void beforeAddAccountAdvice() {
         System.out.println("\n=========> Executing @Before advice on addAccount()");
     }
