7be61d4f5656e7d0a6e51c5290f01766e6bee98c
nima ghaferi <nima.ghaferi@gmail.com>
2022-11-11 18:14:07 +0330

    Spring AOP point cut declaration

--- spring-demo-aop/src/com/nemo/aopdemo/aspect/DemoLoggingAspect.java
+++ spring-demo-aop/src/com/nemo/aopdemo/aspect/DemoLoggingAspect.java
@@ -2,19 +2,24 @@ package com.nemo.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class DemoLoggingAspect {
-    @Before("execution(boolean add*())")
+
+    @Pointcut("execution(* add*())")
+    private void allAddMethods() {
+
+    }
+
+    @Before("allAddMethods()")
     //@Before("execution(boolean add*(java.lang.String))")----match with specific arg type
     //@Before("execution(boolean add*())")-----match with no arg method
     //@Before("execution(boolean add*(*))")-----match with one arg with any type
     //@Before("execution(boolean add*(..))")-----match with any arg number with any types
     //@Before("execution(* com.nemo.aopdemo.dao.*.* (..))")//-----match with any method in a specific class
-
-
     public void beforeAddAccountAdvice() {
         System.out.println("\n=========> Executing @Before advice on addAccount()");
     }
