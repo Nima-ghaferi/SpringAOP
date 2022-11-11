0a73570ae349136362ffffad11852f943b8beea2
nima ghaferi <nima.ghaferi@gmail.com>
2022-11-11 16:23:01 +0330

    init Spring AOP prepare project

---
+++ spring-demo-aop/src/com/nemo/aopdemo/DemoConfig.java
@@ -0,0 +1,12 @@
+package com.nemo.aopdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.nemo.aopdemo")
+public class DemoConfig {
+
+}
---
+++ spring-demo-aop/src/com/nemo/aopdemo/MainDemoApp.java
@@ -0,0 +1,13 @@
+package com.nemo.aopdemo;
+
+import com.nemo.aopdemo.dao.AccountDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainDemoApp {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+        AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+        accountDAO.addAccount();
+        context.close();
+    }
+}
